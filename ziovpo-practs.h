98430fac42c2e2909476cdb8214c7d929858a2f9
thereal599 <lol1lel@mail.ru>
2026-04-11 01:30:20 +0300

    1 - basic app

---
+++ ziovpo-practs.h
@@ -0,0 +1,8 @@
+﻿// ziovpo-practs.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
